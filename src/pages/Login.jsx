619827c6a0ebd37670130732463828a8fb5b336f
Nikunj Vadher <nikunjjv27@gmail.com>
2023-04-18 10:18:17 +0530

    completed with some errors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/apiCalls";
 
 const Container = styled.div`
   width: 100vw;
@@ -49,6 +52,10 @@ const Button = styled.button`
   background-color: teal;
   color: white;
   cursor: pointer;
+  &:disabled{
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 const Link = styled.a`
   /* margin: 5px 0; */
@@ -57,16 +64,32 @@ const Link = styled.a`
   cursor: pointer;
   color: teal;
 `;
+
+const Error = styled.span`
+  color: red;
+
+`
+
 const Login = () => {
+  const [username , setUsername] = useState("")
+  const [password , setPassword] = useState("")
+  const dispatch = useDispatch();
+  const {isFetching , error} = useSelector(state=> state.user)
+
+  const handleClick = e => {
+    e.preventDefault();
+    login(dispatch , {username,password})
+  }
+
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
-
+          <Input placeholder="username" onChange={e=> setUsername(e.target.value)}/>
+          <Input placeholder="password" type="password" onChange={e=> setPassword(e.target.value)}/>
+          <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+          {error && <Error>Something went Wrong....</Error>}
           <Link>Forgot Your Password?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:5000/api/'
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjQ2YzRhYTg0NDEyMTk5MDVhZDJkOSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDc3NDg5NiwiZXhwIjoxNjgxMDM0MDk2fQ.vC-BVi_jWWMb-kd-1rbdzkZd0bYCC78ONcKdDuAyHKU";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser).accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
@@ -9,5 +9,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header: {token: `Bearer ${TOKEN}`}
+    headers: {token: `Bearer ${TOKEN}`}
 })
